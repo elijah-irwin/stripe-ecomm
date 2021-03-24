8ee806c8f40bef9f524dc5407d40cb52c0774a50
Mckenzie Irwin <irwinmck@gmail.com>
2021-03-24 19:12:18 -0400

    Added simple express server to handle the stripe payments.

--- server.js
+++ server.js
@@ -1,3 +1,36 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const path = require('path')
+if (process.env.NODE_ENV !== 'production') require('dotenv').config();
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
+
+const app = express()
+const port = process.env.PORT || 5000;
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'client/build')))
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'))
+  })
+}
+
+app.post('/payment', (req, res) => {
+  const stripePayload = {
+    source: req.body.token.id,
+    amount: req.body.amount,
+    currency: 'cad'
+  }
+
+  stripe.charges.create(stripePayload, (err, response) => {
+    if (err) return res.status(500).send({ error: err })
+    res.send({ success: response })
+  })
+})
+
+app.listen(port, error => {
+  if (error) throw error;
+  console.log(`Server running on port: ${PORT}`)
+})
