225b33df226d67ac7b4f3436da2f476d505dc5fc
Mckenzie Irwin <irwinmck@gmail.com>
2021-03-25 23:36:48 -0400

    Some PWA stuff.

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const path = require('path')
+const enforce = require('express-sslify')
 if (process.env.NODE_ENV !== 'production') require('dotenv').config();
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
 
@@ -12,10 +13,15 @@ app.use(bodyParser.urlencoded({ extended: true }))
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, 'client/build')))
+  app.use(enforce.HTTPS({ trustProtoHeader: true }))
   app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'client/build', 'index.html'))
   })
 }
+
+app.get('/service-worker.js', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '..', 'build', 'service-worker.js'))
+})
 
 app.post('/payment', (req, res) => {
   const stripePayload = {
